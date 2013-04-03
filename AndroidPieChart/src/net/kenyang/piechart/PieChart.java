79d3410e4fded5b3c4a579e369f82a85c6c584fb
Ken Yang <kuas216083@gmail.com>
2013-04-03 17:02:05 +0800

    First commit

---
+++ AndroidPieChart/src/net/kenyang/piechart/PieChart.java
@@ -0,0 +1,192 @@
+/*
+ * Copyright 2013 Ken Yang
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *   
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.kenyang.piechart;
+
+import java.util.ArrayList;
+
+import net.kenyang.androidpiechart.R;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class PieChart extends View {
+
+	private static final String TAG = PieChart.class.getName();
+	private static final int DEGREE_360 = 360;
+	private static String[] PIE_COLORS 	= null;
+	private static int iColorListSize 	= 0;
+	
+	
+	private Paint paintPieFill;
+	private Paint paintPieBorder;
+	private ArrayList<Float> alPercentage = new ArrayList<Float>();
+
+	private int iDisplayWidth, iDisplayHeight;
+	private int iSelectedIndex 	= -1;
+	private int iCenterWidth 	= 0;
+	private int iShift 			= 0;
+	private int iMargin 		= 0;  // margin to left and right, used for get Radius
+	private int iDataSize		= 0;
+	private RectF r = null;
+
+	private float fDensity 		= 0.0f;
+	private float fStartAngle 	= 0.0f;
+	private float fEndAngle 	= 0.0f;
+
+	public PieChart(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		PIE_COLORS = getResources().getStringArray(R.array.colors);
+		iColorListSize = PIE_COLORS.length;
+			
+		fnGetDisplayMetrics(context);
+		iShift 	= (int) fnGetRealPxFromDp(30);
+		iMargin = (int) fnGetRealPxFromDp(40);
+		
+		paintPieFill = new Paint(Paint.ANTI_ALIAS_FLAG);
+		paintPieFill.setStyle(Paint.Style.FILL);
+
+		paintPieBorder = new Paint(Paint.ANTI_ALIAS_FLAG);
+		paintPieBorder.setStyle(Paint.Style.STROKE);
+		paintPieBorder.setStrokeWidth(fnGetRealPxFromDp(3));
+		paintPieBorder.setColor(Color.WHITE);
+		Log.e(TAG, "PieChart init");
+
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		Log.e(TAG, "Before draw==data size:"+iDataSize);
+		
+		for (int i = 0; i < iDataSize; i++) {
+			
+			// check whether the data size larger than color list size
+			if (i>=iColorListSize){
+				paintPieFill.setColor(Color.parseColor(PIE_COLORS[i%iColorListSize]));
+			}else{
+				paintPieFill.setColor(Color.parseColor(PIE_COLORS[i]));
+			}
+			
+			fEndAngle = alPercentage.get(i);
+
+			// convert percentage to angle
+			fEndAngle = fEndAngle / 100 * DEGREE_360;
+
+			// if the part of pie was selected then change the coordinate
+			if (iSelectedIndex == i) {
+				canvas.save(Canvas.MATRIX_SAVE_FLAG);
+				float fAngle = fStartAngle + fEndAngle / 2;
+				double dxRadius = Math.toRadians((fAngle + DEGREE_360) % DEGREE_360);
+				float fY = (float) Math.sin(dxRadius);
+				float fX = (float) Math.cos(dxRadius);
+				canvas.translate(fX * iShift, fY * iShift);
+			}
+
+			canvas.drawArc(r, fStartAngle, fEndAngle, true, paintPieFill);
+			
+			// if the part of pie was selected then draw a border
+			if (iSelectedIndex == i) {
+				canvas.drawArc(r, fStartAngle, fEndAngle, true, paintPieBorder);
+				canvas.restore();
+			}
+			fStartAngle = fStartAngle + fEndAngle;
+		}
+		Log.e(TAG, "End draw");
+	}
+	
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+		// get screen size
+		iDisplayWidth = MeasureSpec.getSize(widthMeasureSpec);
+		iDisplayHeight = MeasureSpec.getSize(heightMeasureSpec);
+		
+		if (iDisplayWidth>iDisplayHeight){
+			iDisplayWidth = iDisplayHeight;
+		}
+
+		/*
+		 *  determine the rectangle size
+		 */
+		iCenterWidth = iDisplayWidth / 2; 
+		int iR = iCenterWidth-iMargin;
+		if (r == null) {
+			r = new RectF(iCenterWidth-iR,  // top
+					iCenterWidth-iR,  		// left
+					iCenterWidth+iR,  		// rights
+					iCenterWidth+iR); 		// bottom
+		}
+		setMeasuredDimension(iDisplayWidth, iDisplayWidth);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+
+		// get degree of the touch point
+		double dx = Math.atan2(event.getY() - iCenterWidth, event.getX() - iCenterWidth);
+		float fDegree = (float) (dx / (2 * Math.PI) * DEGREE_360);
+		fDegree = (fDegree + DEGREE_360) % DEGREE_360;
+
+		// get the percent of the selected degree
+		float fSelectedPercent = fDegree * 100 / DEGREE_360;
+
+		float fTotalPercent = 0;
+		for (int i = 0; i < iDataSize; i++) {
+			fTotalPercent += alPercentage.get(i);
+			if (fTotalPercent > fSelectedPercent) {
+				iSelectedIndex = i;
+				break;
+			}
+		}
+		invalidate();
+		return super.onTouchEvent(event);
+	}
+	
+	private void fnGetDisplayMetrics(Context cxt){
+        final DisplayMetrics dm = cxt.getResources().getDisplayMetrics();
+        fDensity = dm.density;
+    }
+	
+	private float fnGetRealPxFromDp(float fDp){
+		return (fDensity!=1.0f) ? fDensity*fDp : fDp;
+	}
+
+	public void setAdapter(ArrayList<Float> alPercentage) throws Exception {
+		this.alPercentage = alPercentage;
+		iDataSize = alPercentage.size();
+		int iSum = 0;
+		for (int i = 0; i < iDataSize; i++) {
+			iSum+=alPercentage.get(i);
+		}
+		if (iSum!=100){
+			Log.e(TAG,"total percentage is not equl to 100%");
+			iDataSize = 0;
+			throw new Exception();
+		}
+		
+	}
+
+}
